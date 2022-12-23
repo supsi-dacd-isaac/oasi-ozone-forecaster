2a920484a4d770f17d0a4451c62fe80cca52fe0d
dstreppa <davide.strepparava@supsi.ch>
2022-12-23 15:12:17 +0100

    Improved dataset checker script

--- dataset_checker.py
+++ dataset_checker.py
@@ -4,10 +4,12 @@ import pandas as pd
 
 # Example:
 # python dataset_checker.py -i output_pm10/SPC_MOR_20211201_20211205/SPC_MOR_20211201_20211205_dataset.csv
+# python dataset_checker.py -i output_pm10/SPC_MOR_20211201_20211205/SPC_MOR_20211201_20211205_dataset.csv -v
 
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('-i', required=True)
+    arg_parser.add_argument('-v', action='store_const', const=True, default=False)
     args = arg_parser.parse_args()
 
     logger = logging.getLogger()
@@ -16,18 +18,31 @@ if __name__ == "__main__":
 
     input = args.i
 
-    df1 = pd.read_csv(input)
+    df = pd.read_csv(input)
+    df = df.set_index('date')
 
-    nans = df1.isnull().any()
+    print('\nSTARTING PROGRAM')
 
-    print('STARTING PROGRAM')
+    if args.v is True:
+        print('\nSIGNALS WITH NANS:')
 
-    cnt_nans = 1
-    print('INPUT WITH NANS:')
-    for i in range(0, len(nans)):
-        if nans[i] == True:
-            print(cnt_nans, nans.index[i], nans[i])
-            cnt_nans += 1
+    cnt_nans_cases = 1
+    for k in df.columns:
+        cnt_nans = df[k].isna().sum()
+        if cnt_nans > 0:
+            if args.v is True:
+                print('N. %i -> NANS(%s) = %i/%i (%.0f%%)' % (cnt_nans_cases, k, cnt_nans, len(df), cnt_nans*1e2/len(df)))
+            cnt_nans_cases += 1
 
-    print('ENDING PROGRAM')
+    if args.v is True:
+        print('\nDAYS WITH NANS:')
+        days_with_nans = df[df.isna().any(axis=1)].index
+        for day in days_with_nans:
+            print(day)
 
+    print('\nFIRST DAY: %s' % df.head(1).index[0])
+    print('LAST DAY: %s' % df.tail(1).index[0])
+    print('INPUT SIGNALS WITHOUT NANS: %i/%i' % (len(df.columns)-cnt_nans_cases, len(df.columns)))
+    print('DAYS WITHOUT NANS: %i/%i (%.0f%%)' % (len(df.dropna()), len(df), len(df.dropna())*1e2/len(df)))
+
+    print('\nENDING PROGRAM\n')
